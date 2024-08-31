4ed5e444883a7f4e330f9bd38bd59743d9001767
Ashirbek Rajapboyev <98645245+Ashirbek0918@users.noreply.github.com>
2024-08-31 17:34:43 +0500

    fixeddd

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -22,7 +22,7 @@ class AttendanceController extends Controller
     public function create(Request $request)
     {
         $data = $request->all();
-        Log::info('request',$data['data']->DeviceName);
+        Log::info('request',$data['data']['DeviceName']);
 
 //        $data = $request->validated();
 //        $id = $data['EmployeeID'];
