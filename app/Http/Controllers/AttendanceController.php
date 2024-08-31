9f0c032eea61d39ffb1c2d49da0f979fd535d206
Ashirbek Rajapboyev <98645245+Ashirbek0918@users.noreply.github.com>
2024-08-31 18:06:24 +0500

    fixed

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -31,12 +31,12 @@ class AttendanceController extends Controller
             $id = $data['EmployeeID'];
             try {
                 if ($data['PersonGroup'] == 'student') {
-                    Log::info('info' , $data['PersonGroup']);
+                    Log::log('info' , $data['PersonGroup']);
 //                    $student = Student::query()->where('hemis_id', '=', $id)->first();
 //                    $attendance = $this->createAttendance($student, $data, 'student');
 //                    event(new StudentAttendanceCreated($attendance));
                 } elseif ($data['PersonGroup'] == 'teacher' || $data['PersonGroup'] == 'employee') {
-                    Log::info('info' , $data['PersonGroup']);
+                    Log::log('info' , $data['PersonGroup']);
 //                    $teacher = Teacher::query()->where('hemis_id', $id)->first();
 //                    $attendance = $this->createAttendance($teacher, $data, 'teacher');
 //                    event(new TeacherAttendanceCreated($attendance));
