c78a85b75263064bbc632361531fd65a4e903dfe
Allayar Tursimuratov <galaxyasamsung979@gmail.com>
2024-10-05 19:51:03 +0500

    add userslog to dashboard && modified studentAttendance method

--- app/Http/Controllers/web/DashboardController.php
+++ app/Http/Controllers/web/DashboardController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
+use App\Models\UsersLog;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    public function index():View
+    public function index(Request $request):View
     {
-        return view('dashboard.home');
+        $date = $request->input('date') ?: Carbon::today()->toDateString();
+
+        $users = UsersLog::query()->whereDate('date_time', $date)->orderByDesc('date_time')->paginate(20);
+
+        return view('dashboard.home', ['users' => $users, 'selectedDate' => $date]);
     }
 }
